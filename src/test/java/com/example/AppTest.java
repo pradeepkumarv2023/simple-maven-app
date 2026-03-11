4d752a1b2f061e28f34cf340249f85bca70fdfb5
pradeepkumar.v <pradeepkumar.v2023@vitstudent.ac.in>
2026-03-11 14:47:13 +0530

    Add unit tests for multiply and divide methods

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -7,11 +7,25 @@ public class AppTest {
 
     @Test
     public void testAdd() {
-
         App app = new App();
-
-        assertEquals(5, app.add(2, 3));
-
+        assertEquals(5, app.add(2,3));
     }
 
+    @Test
+    public void testSubtract() {
+        App app = new App();
+        assertEquals(1, app.subtract(3,2));
+    }
+
+    @Test
+    public void testMultiply() {
+        App app = new App();
+        assertEquals(6, app.multiply(2,3));
+    }
+
+    @Test
+    public void testDivide() {
+        App app = new App();
+        assertEquals(2, app.divide(4,2));
+    }
 }
