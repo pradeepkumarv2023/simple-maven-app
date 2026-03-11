318e9ab52d204d09fa440a1733695096e662aaa0
pradeepkumar.v <pradeepkumar.v2023@vitstudent.ac.in>
2026-03-11 14:46:35 +0530

    Add arithmetic methods: subtract, multiply, divide

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -6,4 +6,16 @@ public class App {
         return a + b;
     }
 
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+
 }
